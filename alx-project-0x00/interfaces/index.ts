ec9c65b85567296c2705334ce21a12be235286e8
Hemen Solomon <hemensolomon83@gmail.com>
2025-07-20 11:08:14 +0300

    final touch

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,8 @@
 export interface PillProps { // to adjust the pill component to accept the prop title
   title: string
 }
+
+export interface ButtonProps { //defines the type of props that button component expects.
+  title: string; 
+  styles?: string; // optional styles string for custom classes
+}
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,13 +1,23 @@
-import Card from "@/components/Card"
-const Landing: React.FC =  () => {
+import Card from "@/components/Card";
+import Button from "@/components/Button";
+
+const landing: React.FC = () => {
   return (
     <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+
+      <div className="mb-6 space-x-4">
+        <Button title="Small Rounded" styles="text-sm rounded-sm" />
+        <Button title="Medium Rounded" styles="text-base rounded-md" />
+        <Button title="Large Rounded" styles="text-lg rounded-full" />
+      </div>
+
       <Card />
       <Card />
       <Card />
       <Card />
     </div>
-  )
-}
-export default Landing
+  );
+};
+
+export default landing;
